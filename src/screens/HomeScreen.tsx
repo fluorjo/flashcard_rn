84e5bfc001a3e3c9cb1db57f8df67401ec977024
fluorjo <fluorjo@naver.com>
2023-10-13 03:32:46 +0900

    경고문 해결

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,29 @@
 import React, {useState, useCallback, useEffect, useRef} from 'react';
-import {StyleSheet, FlatList, Button, TouchableOpacity, Text} from 'react-native';
+import {
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import {SafeAreaView, View, UnderlineText, TopBar} from '../theme/navigation';
-import { NavigationProp } from '@react-navigation/native';
+import {NavigationProp} from '@react-navigation/native';
 
 type HomeScreenProps = {
   navigation: NavigationProp<{}>;
 };
 
-const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) =>  {
+const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
   return (
     <SafeAreaView>
       <TouchableOpacity onPress={() => console.log('Button clicked')}>
-        <Text onPress={()=>navigation.navigate('LeftScreen')}>LeftScreen</Text>
+        <Text onPress={() => navigation.navigate('LeftScreen' as never)}>
+          LeftScreen
+        </Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
-}
+};
 
 export default HomeScreen;
 
