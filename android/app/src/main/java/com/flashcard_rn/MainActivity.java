d95081e932ef706c9e11d959548298471bbdff2d
fluorjo <fluorjo@naver.com>
2023-10-15 05:28:15 +0900

    reset

--- android/app/src/main/java/com/flashcard_rn/MainActivity.java
+++ android/app/src/main/java/com/flashcard_rn/MainActivity.java
@@ -29,8 +29,5 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(null);
-}
+
 }
