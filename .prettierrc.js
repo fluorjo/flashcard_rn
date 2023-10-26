c7df23f482d6ebec8fcca6e25e9e003831c65ed4
fluorjo <fluorjo@naver.com>
2023-10-26 12:44:20 +0900

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
   bracketSpacing: false,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi:false,
 };
