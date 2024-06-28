434186cdd9da6b4a93982ec571a418d6180e5725
fluorjo <fluorjo@naver.com>
2024-06-28 10:15:17 +0900

    카드 아이템 스타일링 시작

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 interface CardItemProps {
   card: {
@@ -8,14 +8,24 @@ interface CardItemProps {
     correctAnswer: string;
   };
 }
-
 const CardItem: React.FC<CardItemProps> = ({ card }) => {
   return (
-    <View>
+    <View style={styles.Card}>
       <Text>{card.question}</Text>
-      <Text>{card.correctAnswer}</Text>
+       {/* 생각해보니까 이렇게 바로 문제랑 정답이 다 나오면 안되겠구나.  */}
+      {/* <Text>{card.correctAnswer}</Text> */}
     </View>
   );
 };
 
 export default CardItem;
+
+const styles = StyleSheet.create({
+  Card:{
+    width: '90%',
+    height:46,
+    borderWidth:1,
+    backgroundColor:'#ffffff',
+    borderRadius:5,
+  }
+})
